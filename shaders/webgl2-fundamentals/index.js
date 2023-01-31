d380f367a38d63ae3aa61a77a3dcbf2ed73c18b5
Maurice Flanagan <mauricef@gmail.com>
2023-01-31 16:15:10 -0500

    Refactor to use twgl

--- shaders/webgl2-fundamentals/index.js
+++ shaders/webgl2-fundamentals/index.js
@@ -1,28 +1,3 @@
-function createShader(gl, type, source) {
-    var shader = gl.createShader(type)
-    gl.shaderSource(shader, source)
-    gl.compileShader(shader)
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-    if (success) {
-        return shader
-    }
-    console.log(gl.getShaderInfoLog(shader))
-    gl.deleteShader(shader)
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram()
-    gl.attachShader(program, vertexShader)
-    gl.attachShader(program, fragmentShader)
-    gl.linkProgram(program)
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS)
-    if (success) {
-        return program
-    }
-    console.log(gl.getProgramInfoLog(program))
-    gl.deleteProgram(program)
-}
-
 async function fetchText(url) {
     var response = await fetch(url)
     var text = await response.text()
@@ -39,57 +14,38 @@ function setRectangle(gl, x, y, width, height) {
     var y1 = y
     var y2 = y + height
 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2
-    ]), gl.STATIC_DRAW)
+    return twgl.createBufferInfoFromArrays(gl, {
+            a_position: {
+                numComponents: 2,
+                data: new Float32Array([
+                    x1, y1,
+                    x2, y1,
+                    x1, y2,
+                    x1, y2,
+                    x2, y1,
+                    x2, y2
+                ])
+            }
+        })
 }
 
 async function run() {
     /** @type {HTMLCanvasElement} */
     var canvas = document.querySelector("#c")
     var gl = canvas.getContext("webgl2", {preserveDrawingBuffer:true})
-    var vertexShaderSource = await fetchText("./main.vert")
-    var fragmentShaderSource = await fetchText("./main.frag")
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
-    var program = createProgram(gl, vertexShader, fragmentShader)
-    var positionBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-    var vao = gl.createVertexArray()
-    gl.bindVertexArray(vao)
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position")
-    gl.enableVertexAttribArray(positionAttributeLocation)
-    var size = 2
-    var type = gl.FLOAT
-    var normalize = false
-    var stride = 0
-    var offset = 0
-    gl.vertexAttribPointer(
-        positionAttributeLocation,
-        size,
-        type,
-        normalize,
-        stride,
-        offset
-    )
+    var vss = await fetchText("./main.vert")
+    var fss = await fetchText("./main.frag")
+    var programInfo = twgl.createProgramInfo(gl, [vss, fss])
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");    
-    gl.useProgram(program)
-    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
-    gl.bindVertexArray(vao)
-    var colorLocation = gl.getUniformLocation(program, "u_color")
     function render() {
-        setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300))
-        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
-        var primitiveType = gl.TRIANGLES
-        var offset = 0
-        var count = 6
-        gl.drawArrays(primitiveType, offset, count)
+        var bufferInfo = setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300))
+        gl.useProgram(programInfo.program)
+        twgl.setUniforms(programInfo, {
+            u_resolution: [gl.canvas.width, gl.canvas.height],
+            u_color: [Math.random(), Math.random(), Math.random(), 1]
+        })
+        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
+        twgl.drawBufferInfo(gl, bufferInfo)
         requestAnimationFrame(render)
     }
     requestAnimationFrame(render)
